dec8caaa11b3d89e04e8f3d44c75a35f83dc66c9
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-26 21:46:56 -1000

    Just added users

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -6,3 +6,18 @@ CREATE USER has_many_user ;
 CREATE DATABASE has_many_blogs WITH OWNER has_many_blogs;
 
 \c has_many_blogs
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS comments;
+
+
+CREATE TABLE users (
+  id SERIAL NOT NULL PRIMARY KEY,
+  username VARCHAR(90) NOT NULL,
+  first_name VARCHAR(90) DEFAULT NULL,
+  last_name VARCHAR(90) DEFAULT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+ );
+ 
