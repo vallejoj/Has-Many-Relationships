3ed8416b032343eb03658239debc1a981b763f3f
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-27 19:21:39 -1000

    Done with 5

--- joins.sql
+++ joins.sql
@@ -1,3 +1,16 @@
 SELECT * FROM users;
 
-SELECT * FROM posts WHERE  users_id = 100;
+SELECT * FROM posts WHERE users_id = 100;
+
+SELECT posts.*, users.first_name, users.last_name
+FROM posts JOIN users ON posts.users_id = users.id
+WHERE users_id = 100;
+
+SELECT posts.*, users.username
+FROM posts INNER JOIN users ON posts.users_id = users.id
+WHERE users.first_name = 'Norene' AND users.last_name = 'Schmitt';
+
+
+SELECT users.username
+FROM users
+WHERE created_at > '2015-01-01';
