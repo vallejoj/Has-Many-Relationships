fbef50137423ddd5dcffcf6795a4317969cb7242
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-27 20:48:06 -1000

    Fixed query 3

--- joins.sql
+++ joins.sql
@@ -6,7 +6,7 @@ SELECT * FROM posts WHERE users_id = 100;
 --3
 SELECT posts.*, users.first_name, users.last_name
 FROM posts JOIN users ON posts.users_id = users.id
-WHERE users_id = 100;
+WHERE users_id = 200;
 --4
 SELECT posts.*, users.username
 FROM posts INNER JOIN users ON posts.users_id = users.id
