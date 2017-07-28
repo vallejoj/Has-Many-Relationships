ab14abb642e217984bde9ed980b08e9315475223
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-27 21:06:07 -1000

    fixed queires.

--- joins.sql
+++ joins.sql
@@ -14,12 +14,12 @@ WHERE users.first_name = 'Norene' AND users.last_name = 'Schmitt';
 --5
 
 SELECT users.username
-FROM users
-WHERE created_at > '2015-01-01';
+FROM posts JOIN users ON posts.users_id = users.id
+WHERE posts.created_at >'2015-01-01';
 --6
 SELECT posts.title, posts.content, users.username
 FROM posts JOIN users ON posts.users_id = users.id
-WHERE posts.created_at > '2015-01-01';
+WHERE posts.created_at <'2015-01-01';
 --7
 SELECT comments.*, posts.title AS "Post Title"
 FROM comments JOIN posts ON comments.users_id = posts.id;
@@ -27,14 +27,14 @@ FROM comments JOIN posts ON comments.users_id = posts.id;
 --8
 
 SELECT comments.*, posts.title AS "Post Title", posts.url AS "post_url", comments.body AS "comment_body"
-FROM comments JOIN posts ON comments.users_id = posts.id
+FROM posts JOIN comments ON   posts.id = comments.users_id
 WHERE posts.created_at < '2015-01-01';
 
 --9
 
 SELECT comments.*, posts.title AS "Post Title", posts.url AS "post_url", comments.body AS "comment_body"
-FROM comments JOIN posts ON comments.users_id = posts.id
-WHERE posts.created_at < '2015-01-01';
+FROM posts JOIN comments ON posts.id = comments.users_id
+WHERE posts.created_at > '2015-01-01';
 
 --10
 
